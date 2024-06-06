984acff7dba3f150632714cf383dd897b138950c
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2024-06-06 10:37:15 +0100

    Update examples_a.m

--- session_2/examples_a.m
+++ session_2/examples_a.m
@@ -13,29 +13,17 @@ legend('L(u)','dL/du','minimum')
 
 %% Example 1
 f =@(x1,x2) x1.^2+x2.^2;
-df =@(x1,x2) 2*x1+2*x2;
 figure
 fsurf(f,[-30 30],'FaceAlpha',0.5,'ShowContours','on')
-hold on
-fsurf(df,[-30 30],'FaceAlpha',0.05,'ShowContours','on')
 
 f =@(x1,x2) -x1.^2-x2.^2;
-df =@(x1,x2) -2*x1-2*x2;
 figure
 fsurf(f,[-30 30],'FaceAlpha',0.5,'ShowContours','on')
-hold on
-fsurf(df,[-30 30],'FaceAlpha',0.05,'ShowContours','on')
 
 f =@(x1,x2) x1.^2-x2.^2;
-df =@(x1,x2) 2*x1-2*x2;
 figure
 fsurf(f,[-30 30],'FaceAlpha',0.5,'ShowContours','on')
-hold on
-fsurf(df,[-30 30],'FaceAlpha',0.05,'ShowContours','on')
 
 f =@(x1,x2) -x1.^2+x2.^2;
-df =@(x1,x2) -2*x1+2*x2;
 figure
 fsurf(f,[-30 30],'FaceAlpha',0.5,'ShowContours','on')
-hold on
-fsurf(df,[-30 30],'FaceAlpha',0.05,'ShowContours','on')
